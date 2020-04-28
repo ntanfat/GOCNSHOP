7d1588136ffaf33c9c6092287a15420f807a5f87
F4T <thekidbandes@gmail.com>
2020-04-29 02:25:06 +0700

    Make public

--- onload.js
+++ onload.js
@@ -4,11 +4,11 @@ var _nhCurrentTime = (new Date().getTime());
 var _nhCssElement = document.createElement('link');
 _nhCssElement.type = "text/css";
 _nhCssElement.rel = "stylesheet";
-_nhCssElement.href = _nhDomain + 'toolbar.css?token=ACZYWCLYZLKGEKIRHJKZNOC6VCASO&v=' + _nhCurrentTime;
+_nhCssElement.href = _nhDomain + 'toolbar.css?v=' + _nhCurrentTime;
 
 var _nhJsElement = document.createElement("script");
 _nhJsElement.type = "text/javascript";
-_nhJsElement.src = _nhDomain + 'toolbar.js?token=ACZYWCPKKL3SFRK32MOCEX26VCATC&v=' + _nhCurrentTime;
+_nhJsElement.src = _nhDomain + 'toolbar.js?v=' + _nhCurrentTime;
 
 document.body.appendChild(_nhCssElement);
 document.body.appendChild(_nhJsElement);
