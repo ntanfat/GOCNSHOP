85550afc1a1c2aa36a7e5288867ad16d2a38388f
F4T <thekidbandes@gmail.com>
2020-04-29 02:21:45 +0700

    Updated domain

--- onload.js
+++ onload.js
@@ -1,14 +1,14 @@
-var _nhDomain = "//nhaphangtrungquoc.vn/";
+var _nhDomain = "https://raw.githubusercontent.com/ntanfat/taobao/master/";
 var _nhCurrentTime = (new Date().getTime());
 
-var _nhCssElement =document.createElement('link');
+var _nhCssElement = document.createElement('link');
 _nhCssElement.type = "text/css";
 _nhCssElement.rel = "stylesheet";
-_nhCssElement.href = _nhDomain+'js/toolbar/toolbar.css?v=' + _nhCurrentTime;
+_nhCssElement.href = _nhDomain + 'toolbar.css?token=ACZYWCLYZLKGEKIRHJKZNOC6VCASO&v=' + _nhCurrentTime;
 
 var _nhJsElement = document.createElement("script");
 _nhJsElement.type = "text/javascript";
-_nhJsElement.src = _nhDomain+'js/toolbar/toolbar.js?v=' + _nhCurrentTime;
+_nhJsElement.src = _nhDomain + 'toolbar.js?token=ACZYWCPKKL3SFRK32MOCEX26VCATC&v=' + _nhCurrentTime;
 
 document.body.appendChild(_nhCssElement);
 document.body.appendChild(_nhJsElement);
