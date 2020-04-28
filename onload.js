22ff49377d2d1c03195c786ec73ea904addb976e
F4T <thekidbandes@gmail.com>
2020-04-29 02:43:32 +0700

    1

--- onload.js
+++ onload.js
@@ -5,12 +5,12 @@ var _nhCssElement = document.createElement('link');
 _nhCssElement.type = "text/css";
 _nhCssElement.rel = "stylesheet";
 //_nhCssElement.href = _nhDomain + 'toolbar.css?v=' + _nhCurrentTime;
-_nhCssElement.href = 'https://rawcdn.githack.com/ntanfat/taobao/7d1588136ffaf33c9c6092287a15420f807a5f87/toolbar.css';
+_nhCssElement.href = 'https://rawcdn.githack.com/ntanfat/taobao/ee49d12dcab192cf9f4e58ff25f530817c071f5f/toolbar.css';
 
 var _nhJsElement = document.createElement("script");
 _nhJsElement.type = "text/javascript";
 //_nhJsElement.src = _nhDomain + 'toolbar.js?v=' + _nhCurrentTime;
-_nhJsElement.src = 'https://rawcdn.githack.com/ntanfat/taobao/7d1588136ffaf33c9c6092287a15420f807a5f87/toolbar.js';
+_nhJsElement.src = 'https://rawcdn.githack.com/ntanfat/taobao/ee49d12dcab192cf9f4e58ff25f530817c071f5f/toolbar.js';
 
 document.body.appendChild(_nhCssElement);
 document.body.appendChild(_nhJsElement);
