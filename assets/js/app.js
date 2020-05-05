ebcedab1dd095d48efcd09a8dbc3d897ba4ca78b
F4T <thekidbandes@gmail.com>
2020-05-05 21:22:47 +0700

    1

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var _nhDomain = "//nhaphangtrungquoc.vn/";
+var _nhCurrentTime = (new Date().getTime());
+
+var _nhCssElement = document.createElement('link');
+_nhCssElement.type = "text/css";
+_nhCssElement.rel = "stylesheet";
+_nhCssElement.href = _nhDomain + 'js/toolbar/toolbar.css?v=' + _nhCurrentTime;
+
+var _nhJsElement = document.createElement("script");
+_nhJsElement.type = "text/javascript";
+_nhJsElement.src = _nhDomain + 'js/toolbar/toolbar.js?v=' + _nhCurrentTime;
+
+document.body.appendChild(_nhCssElement);
+document.body.appendChild(_nhJsElement);
