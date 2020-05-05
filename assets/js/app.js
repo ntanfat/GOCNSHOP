76fd0ff879d87f2789d385668b343f51a8ed2339
F4T <thekidbandes@gmail.com>
2020-05-05 21:43:36 +0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
+var timestamp = new Date().getTime();
 var js = document.createElement('script');
 js.type = 'text/javascript';
-js.src = '//nhaphangtrungquoc.vn/js/toolbar/toolbar.js?v=' + (new Date().getTime());
+js.src = 'https://rawcdn.githack.com/ntanfat/taobao/2e876d83e9de829eb686dbb29620874b5bc44998/assets/js/content.js' + timestamp;
 document.body.appendChild(js);
