8b18010f23d54f184859f67e41028227e3309ca9
F4T <thekidbandes@gmail.com>
2020-05-05 21:47:45 +0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var timestamp = new Date().getTime();
 var js = document.createElement('script');
 js.type = 'text/javascript';
-js.src = 'https://rawcdn.githack.com/ntanfat/taobao/2e876d83e9de829eb686dbb29620874b5bc44998/assets/js/content.js' + '?v=' + timestamp;
+js.src = 'https://raw.githack.com/ntanfat/taobao/master/assets/js/content.js' + '?v=' + timestamp;
 document.body.appendChild(js);
