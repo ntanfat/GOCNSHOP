ed6aa9699f1933f3f18a59cf396a141218766e99
F4T <thekidbandes@gmail.com>
2020-05-07 22:39:58 +0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var url = 'https://rawcdn.githack.com/ntanfat/taobao/7279bead210fa656a876dcd2f8c078cfb614a43d/assets/js/content.js';
+var url = 'https://rawcdn.githack.com/ntanfat/taobao/12adf484a7147da2569eeb67a8575a0f9e9bae58/assets/js/content.js';
 var version = '?v=' + new Date().getTime();
 var js = document.createElement('script');
 js.type = 'text/javascript';
